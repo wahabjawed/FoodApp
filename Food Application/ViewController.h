864bd28d76de9a4e7f534845fd56edf549a9672c
Zainu Corporation <ZCorp@Zainus-iMac.local>
2014-07-10 20:26:29 +0500

    Initial Commit

---
+++ Food Application/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Food Application
+//
+//  Created by Zainu Corporation on 10/07/2014.
+//  Copyright (c) 2014 Zainu Corporation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Food ApplicationTests/Food_ApplicationTests.m
@@ -0,0 +1,34 @@
+//
+//  Food_ApplicationTests.m
+//  Food ApplicationTests
+//
+//  Created by Zainu Corporation on 10/07/2014.
+//  Copyright (c) 2014 Zainu Corporation. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Food_ApplicationTests : XCTestCase
+
+@end
+
+@implementation Food_ApplicationTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
